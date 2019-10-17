06cb1c4e3dfab9388211e45feb616c9cc81b92cd
Ayads <ayad.shaif@gmail.com>
2019-10-17 13:05:53 +0200

    impleamented handleBullyMessage

--- Skeleton/src/se/miun/distsys/messages/BullyMessage.java
+++ Skeleton/src/se/miun/distsys/messages/BullyMessage.java
@@ -8,4 +8,22 @@ public class BullyMessage extends Message {
 	public BullyMessage(Integer clientID) {
 		this.clientID = clientID;
 	}
+
+	public void handleBullyMessage(BullyMessage bullyMessage){
+		if(bullyMessage instanceof ElectionRequestMessage) {
+			ElectionRequestMessage electionRequestMessage = (ElectionRequestMessage) bullyMessage;
+
+		} else if(bullyMessage instanceof ElectionResultMessage) {
+			ElectionResultMessage electionResultMessage = (ElectionResultMessage) bullyMessage;
+
+		} else if(bullyMessage instanceof SequenceRequestMessage) {
+			SequenceRequestMessage sequenceRequestMessage = (SequenceRequestMessage) bullyMessage;
+
+		} else if(bullyMessage instanceof SequenceReplyMessage) {
+			SequenceReplyMessage sequenceReplyMessage = (SequenceReplyMessage) bullyMessage;
+
+		} else {
+			System.out.println("Unknown bullyMessage type");
+		}
+	}
 }
